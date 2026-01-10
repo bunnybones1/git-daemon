0c3f22e53d2dc5816d451e1e043e2265cd42d60c
Tomasz Dysinski <dysinski.tomasz@gmail.com>
2026-01-09 23:56:51 -0800

    formatted eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,17 +2,17 @@ module.exports = {
   root: true,
   env: {
     node: true,
-    es2020: true
+    es2020: true,
   },
   parser: "@typescript-eslint/parser",
   plugins: ["@typescript-eslint", "prettier"],
   extends: [
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
-    "plugin:prettier/recommended"
+    "plugin:prettier/recommended",
   ],
   rules: {
-    "@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_" }]
+    "@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
   },
-  ignorePatterns: ["dist/", "node_modules/", "workspace/"]
+  ignorePatterns: ["dist/", "node_modules/", "workspace/"],
 };
